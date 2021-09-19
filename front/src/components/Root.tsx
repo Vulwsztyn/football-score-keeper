4c745760a72bd78d7a25fff48c0c7b306e27f365
Artur Mostowski <artur.mostowski@currency-one.com>
2021-09-19 23:47:58 +0200

    front | Dashboard (#24)

---
+++ front/src/components/Root.tsx
@@ -0,0 +1,27 @@
+import * as React from 'react'
+import Dashboard from './Dashboard'
+import Grid from '@mui/material/Grid'
+
+export default function Root() {
+  return (
+    <div style={{ width: '100%' }}>
+      <Grid
+        container
+        sx={{
+          display: 'flex',
+          justifyContent: 'center',
+          p: 1,
+          m: 1,
+          bgcolor: 'background.paper',
+        }}
+      >
+        <Grid item xs={12} sx={{ display: 'flex', justifyContent: 'center' }}>
+          <h4>Teams & Players</h4>
+        </Grid>
+        <Grid item xs={12} sx={{ maxWidth: 1000 }}>
+          <Dashboard />
+        </Grid>
+      </Grid>
+    </div>
+  )
+}
